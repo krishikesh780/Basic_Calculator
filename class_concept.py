d64b23ba547ff47ee6308dd8aa2f3836acc488bd
krishikesh780 <53981965+krishikesh780@users.noreply.github.com>
2023-06-13 13:26:34 +0000

    5th commit, topics class concept

---
+++ class_concept.py
@@ -0,0 +1,10 @@
+class myClass():
+  def __init__ (self, a,b):
+    self.x=a
+    self.y=b
+  def add(self):
+    print(self.x+self.y)
+  def mult(self):
+    print(self.x*self.y)
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import calculator
-calculator.add()
-calculator.sub()
-calculator.mul()
-calculator.div()
+import function_calculator
+import class_concept
+function_calculator.add()
+function_calculator.sub()
+function_calculator.mul()
+function_calculator.div()
 
 print("This my first project  i.e Basic Calculator ")
+
+e=class_concept.myClass(10,20)
+e.add()
+e.mult()
